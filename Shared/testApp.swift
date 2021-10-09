1fbc79b7c527020b76d95316f6754b98cc4ef2ce
ShomaYamaguchi <b1019125@fun.ac.jp>
2021-10-09 10:24:40 +0900

    Initial Commit

---
+++ Shared/testApp.swift
@@ -0,0 +1,17 @@
+//
+//  testApp.swift
+//  Shared
+//
+//  Created by AICT201305 FUN on 2021/10/09.
+//
+
+import SwiftUI
+
+@main
+struct testApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
